e219911285d623e010626dc4d8c5bb4186084ec5
TARUN <tarun@TARUNs-MacBook-Pro.local>
2025-01-27 16:33:33 +0530

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,21 @@
+import Card from './Card'
+function Tours({tours, removeTour}) {
+
+     console.log("i am in tours");
+    return (
+        <div className='container'>
+            <div>
+                <h2 className='title'> Plan With Tarun</h2>
+            </div>
+            <div className='cards'>
+                {
+                    tours.map( (tour) => {
+                        return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+                    } )
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Tours;
